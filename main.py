4d14322653fb1ef1abc207fca0d297374cd8f0ee
aaron-code26 <aaronpaskins44@gmail.com>
2026-07-14 22:32:56 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+#section 1
+
+name = "Aaron"
+age = 20
+height= 6.00
+is_student=True
+
+print(name,type(name))
+print(age,type(age))
+print (height, type(height))
+print (is_student, type(is_student))
+
+#Section 2
+#name= input("what your name:")
+#User= eval(input("what is your age:"))
+#print("Hello:",name)
+#print("your age:", User)
+
+#Section 3
+#Num= input("enter a number:")
+#Num2=input("enter the second number:")
+#Sum= float(Num)*float(Num2)
+#print(name,"your answer is:",Sum)
+
+#Section 4 
+#print("-------------------")
+#print("-------------------")
+#print("RECEIPT")
+#Book = "Calculus Super Review"
+#Price = float(30)
+#quantity= 1
+#Sum=Price * quantity
+#print('item:',Book)
+#print("price:",Price)
+#print('quantity',quantity)
+#print("Total is:",Sum)
+#print("-------------------")
+
+#Section 5
+name=(input("Enter your name:"))
+Hometown= input("Enter where lived City,State:")
+Hobby= input("Named something that like doing:")
+Funfact= input ('Say something about you that most people dont know:')
+birthyear=int(input('Date of Birth:'))
+currentyear=2026
+Age=currentyear-birthyear
+
+print("Profile:",name)
+print("Hometown:",Hometown)
+print("Funfact:",Funfact)
+print("Age:",Age)
+
+
+
+
+
